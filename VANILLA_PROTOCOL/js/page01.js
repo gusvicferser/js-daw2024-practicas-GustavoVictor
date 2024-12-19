32697e6ad4c4597ef89ec2cfbfb6fea595e06d99
gusvicferser <gusvfs10@gmail.com>
2024-12-19 17:19:51 +0100

    vanilla_clase_19_12

---
+++ VANILLA_PROTOCOL/js/page01.js
@@ -0,0 +1,68 @@
+/**
+ * Aplicación JS para cambiar la página 1 a la introducción del usuario pulsando
+ * una combinación de teclas o bien esperando 5 segundos. También valida el 
+ * usuario en todo momento cuando pierde el foco del input:
+ * 
+ * @author Gustavo Víctor
+ * @version 1.0
+ */
+
+
+import * as utl from './utilities.js';
+
+/**
+ * Función para cambiar la pantalla de bienvenida al formulario de introducción
+ * de usuario:
+ * 
+ * @author Gustavo Victor
+ * @version 2.0
+ */
+function changeInput() {
+
+    document.querySelector('h1').remove();
+
+    let array = ['for:user'];
+
+    let label = utl.newElement('label', array);
+    label.innerText = 'USUARIO';
+
+    array = ['type:text', 'id:user', 'name:user', 'placeholder:Introduce aquí tu usuario'];
+    let input = utl.newElement('input', array);
+
+    array = ['type:button'];
+    let btn = utl.newElement('button', array);
+    btn.innerText = 'Entra';
+
+    let container = document.getElementById('container');
+
+    let br1 = document.createElement('br');
+    let br2 = document.createElement('br');
+
+    utl.introduce(container, label, br1, input, br2, btn);
+
+    let user = document.getElementById('user');
+    let regExp = /^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/;
+
+    utl.checkREx(user, regExp);
+
+}
+
+
+
+// Defino variables:
+var lastKey = '';
+
+// Evento para cuando se pulsan las dos teclas juntas y en ese orden:
+document.addEventListener('keydown', function (event) {
+    if (lastKey === 'Alt' && event.key === 'b') {
+        changeInput();
+    } else {
+        lastKey = event.key;
+    }
+})
+
+// Evento que, alternativamente carga la función pasados 5 segundos:
+window.addEventListener('load', function () {
+    this.setTimeout(changeInput, 5000);
+})
+
---
+++ VANILLA_PROTOCOL/js/utilities.js
@@ -0,0 +1,131 @@
+/**
+ * JS para las utilidades a usar en cada página del proyecto:
+ * 
+ * @author Gustavo Víctor
+ * @version 1.0
+ */
+
+/**
+ * Función para crear un elemento y sus atributos:
+ * 
+ * @param {string} element El elemento que queremos crear: 
+ * @param {array} atributes Un array como un JSON con atributo:valor
+ * 
+ * @author Gustavo Victor
+ * @version 1.0
+ */
+function newElement(domElement, domAtributes) {
+
+    let dom = document.createElement(domElement);
+
+    if (domAtributes != null || domAtributes != '') {
+        domAtributes.forEach(element => {
+            let atribute = element.substring(0, element.search(':'));
+            let value = element.substring(element.search(':') + 1, element.length);
+            dom.setAttribute(atribute, value);
+        });
+    }
+
+    return dom;
+}
+
+/**
+ * Función para meter todos los elementos en un elemento del DOM:
+ * 
+ * @param {string} domElement El elemento en el cual vamos a introducir los elementos 
+ * @param {...*} domElements Array de elementos a introducir
+ */
+function introduce(domElement, ...domElements) {
+
+    if (domElements != null || domElements != '') {
+        domElements.forEach(element => {
+            domElement.appendChild(element);
+        })
+    }
+}
+
+/**
+ * Función para comprobar si un elemento cumple una expresión regular cambiar el 
+ * borde de color:
+ * 
+ * @param {DOM element} domElement Elemento del DOM que queremos verificar
+ * @param {Reg Exp} regExp Una expresión regular para verificar  
+ * 
+ * @author Gustavo Victor
+ * @version 1.0
+*/
+function checkREx(domElement, regExp) {
+    
+    if (domElement != null || domElement != '') {
+        domElement.addEventListener('blur', function () {
+    
+            if (regExp.test(this.value)) {
+                this.style.border = '3px solid green';
+            } else {
+                this.style.border = '3px solid red';
+            }
+        })
+    
+        domElement.addEventListener('focus', function () {
+            this.select();
+            this.style.border = 'none';
+        })
+    }
+
+}
+
+/**
+ * Función proporcionada por la profesora para crear una cookie con JavaScript:
+ * 
+ * @param cname "Nombre de la cookie"
+ * @param cvalue "Valor o texto de la cookie"
+ * @param exdays "Días de duración especificado en int"
+ */
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+
+/**
+ * Función para devolver el valor de una cookie:
+ * 
+ * @param {String} cname 
+ * @returns Valor de la cookie en cuestión
+ */
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+/**
+ * Función para borrar una cookie creada por nosotros:
+ * 
+ * @param cname Nombre de la cookie a borrar
+ */
+function deleteCookie(cname) {
+    document.cookie = cname + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/';
+}
+
+
+// Exportamos el utilities:
+export {
+    newElement,
+    introduce,
+    checkREx,
+    setCookie,
+    getCookie,
+    deleteCookie
+}
