69ee286f756cf7afc3a427f6485d12abf2c0aed7
gusvicferser <gusvfs10@gmail.com>
2025-01-30 17:12:25 +0100

    TS_30_01

--- NodeTest/src/app/app.component.ts
+++ NodeTest/src/app/app.component.ts
@@ -8,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Gustavo Víctor';
+
+  // Ejercicio 1:
+  nombre: string = 'Gustavo';
+
+  // Ejercicio 2:
+  calcularAreaRectangulo(base: number, altura: number) {
+
+    return base*altura;
+  }
+  
+  ngOnInit() {
+    console.log(`Area: ${this.calcularAreaRectangulo(5, 10)}`);
+  }
+
+}
+
+export interface Persona {
+  nombre: string;
+  edad: number;
+  correo: string
 }
